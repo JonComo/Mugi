6cfe54d5f80138529acef0eefe44953e660613e1
joncomo <jon@underground.net>
2015-04-17 12:09:31 -0400

    Initial Commit

---
+++ Mugi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Mugi
+//
+//  Created by Jon Como on 4/17/15.
+//  Copyright (c) 2015 Jon Como. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Mugi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Mugi
+//
+//  Created by Jon Como on 4/17/15.
+//  Copyright (c) 2015 Jon Como. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
